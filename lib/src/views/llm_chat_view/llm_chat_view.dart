7433b8688884399b941dac69dd2d19889a38fe5a
Jeremy <jkrishan@dsgdigital.nl>
2025-10-13 10:44:44 +0200

    do not fill in but send suggestion message

--- lib/src/views/llm_chat_view/llm_chat_view.dart
+++ lib/src/views/llm_chat_view/llm_chat_view.dart
@@ -406,8 +406,10 @@ class _LlmChatViewState extends State<LlmChatView>
     }
   }
 
-  void _onSelectSuggestion(String suggestion) =>
-      setState(() => _initialMessage = ChatMessage.user(suggestion, []));
+  void _onSelectSuggestion(String suggestion) {
+    // setState(() => _initialMessage = ChatMessage.user(suggestion, []));
+    _onSendMessage(suggestion, []);
+  }
 
   void _onHistoryChanged() {
     // if the history is cleared, clear the initial message
